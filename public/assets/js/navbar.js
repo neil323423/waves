c8ef40d1a4e18d5385b4c7d608e14c101bd8e0c2
𓍼 <143974574+xojw@users.noreply.github.com>
2025-01-14 17:29:28 -0600

    whoops

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -1,19 +1,19 @@
 document.getElementById('refreshIcon').addEventListener('click', function() {
-    var iframe = document.querySelector('.frame');
+    var iframe = document.querySelector('.iframe');
     iframe.contentWindow.location.reload(true);
 });
 
 document.getElementById('fullscreenIcon').addEventListener('click', function() {
-    var iframe = document.querySelector('.frame');
+    var iframe = document.querySelector('.iframe');
     iframe.requestFullscreen();
 });
 
 document.getElementById('backIcon').addEventListener('click', function() {
-    var iframe = document.querySelector('.frame');
+    var iframe = document.querySelector('.iframe');
     iframe.contentWindow.history.back();
 });
 
 document.getElementById('forwardIcon').addEventListener('click', function() {
-    var iframe = document.querySelector('.frame');
+    var iframe = document.querySelector('.iframe');
     iframe.contentWindow.history.forward();
 });
