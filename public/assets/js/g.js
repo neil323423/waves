da88b8f01514cf032fcf737f7d5efc007bbd1f2f
sent <143974574+xojw@users.noreply.github.com>
2025-03-24 20:47:51 -0500

    Update g.js

--- public/assets/js/g.js
+++ public/assets/js/g.js
@@ -2,44 +2,45 @@ document.addEventListener('DOMContentLoaded', function() {
 	const searchInput = document.getElementById('gameSearchInput');
 	const grid = document.querySelector('.games-grid');
 	let gamesData = [];
-
+  
 	fetch('/assets/data/g.json')
-		.then(response => response.json())
-		.then(data => {
-			gamesData = data.games;
-			
-			searchInput.placeholder = `Search through ${gamesData.length} Games…`;
+	  .then(response => response.json())
+	  .then(data => {
+		gamesData = data;
 
-			displayGames(gamesData);
-
-			searchInput.addEventListener('input', function() {
-				const query = searchInput.value.toLowerCase();
-				const filteredGames = gamesData.filter(game => {
-					const title = game.title ? game.title.toLowerCase() : '';
-					const description = game.description ? game.description.toLowerCase() : '';
-					return title.includes(query) || description.includes(query);
-				});
-				displayGames(filteredGames);
-			});
-		})
-		.catch(err => console.error('Error loading games data:', err));
-
-	function displayGames(games) {
-		grid.innerHTML = '';
-		if (games.length === 0) {
-			grid.innerHTML = '<p>Zero games were found matching your search :(</p>';
-		}
-		games.forEach(game => {
-			const card = document.createElement('div');
-			card.classList.add('game-card');
-			card.innerHTML = `
-        <img src="${game.icon}" alt="${game.title} Icon" />
-        <h2>${game.title}</h2>
-      `;
-			card.addEventListener('click', function() {
-				window.handleSearch(game.link);
-			});
-			grid.appendChild(card);
+		searchInput.placeholder = `Search through ${gamesData.length} Games…`;
+  
+		displayGames(gamesData);
+  
+		searchInput.addEventListener('input', function() {
+		  const query = searchInput.value.toLowerCase();
+		  const filteredGames = gamesData.filter(game => {
+			const name = game.name ? game.name.toLowerCase() : '';
+			return name.includes(query);
+		  });
+		  displayGames(filteredGames);
 		});
+	  })
+	  .catch(err => console.error('Error loading games data:', err));
+  
+	function displayGames(games) {
+	  grid.innerHTML = '';
+	  if (games.length === 0) {
+		grid.innerHTML = '<p>Zero games were found matching your search :(</p>';
+		return;
+	  }
+	  games.forEach(game => {
+		const card = document.createElement('div');
+		card.classList.add('game-card');
+		card.innerHTML = `
+		  <img src="/assets/g/${game.directory}/${game.image}" alt="${game.name} Icon" />
+		  <h2>${game.name}</h2>
+		`;
+		card.addEventListener('click', async function() {
+		  const url = `${location.origin}/assets/g/${game.directory}`;
+		  await handleSearch(url);
+		});
+		grid.appendChild(card);
+	  });
 	}
-});
+  });
